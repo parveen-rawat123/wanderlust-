43540cd28655623127b7cf636d8136547c92bc90
praveen singh <rawatpraveen477@gmail.com>
2024-02-03 09:33:22 +0530

    create template

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended :true}));
 app.use(methodOverride("_method"));
-app.use('ejs',ejsMate);
+app.engine('ejs',ejsMate);
 
 
 main()
