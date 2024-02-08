0fb57add49421542c2dcb1c5044d97e61a09285a
praveen singh <rawatpraveen477@gmail.com>
2024-02-08 17:09:40 +0530

    handling error

--- models/listing.js
+++ models/listing.js
@@ -10,16 +10,8 @@ const listingSchema = new Schema({
 
     image: {
         type: String,
-        default : "https://unsplash.com/photos/the-sun-is-setting-over-a-rocky-landscape-MV7iHCilMJc",
-        set: function (v) {
-            if (typeof v === 'object' && v.url) {
-                return v.url;
-            } else if (v === "") {
-                return "https://unsplash.com/photos/the-sun-is-setting-over-a-rocky-landscape-MV7iHCilMJc";
-            } else {
-                return v;
-            }
-        }
+        default:"https://images.unsplash.com/photo-1573561507036-b0110570e2e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c3Vuc2V0JTIwYmVhY2h8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60",
+        set: (v)=> v ==="" ? "https://images.unsplash.com/photo-1573561507036-b0110570e2e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c3Vuc2V0JTIwYmVhY2h8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60" : v,
     },
     
     price : Number,
--- schema.js
+++ schema.js
@@ -1,12 +1,12 @@
-const joi = require("joi");
+const Joi = require("joi");
 
-module.exports.listingSchema = joi.object({
-    listing: joi.object({
-        title: joi.string().required(),
-        description: joi.string().required(),
-        location: joi.string().required(),
-        country: joi.string().required(),
-        price: joi.number().required().min(0),
-        image: joi.string().allow(" ",null),
+module.exports.listingSchema = Joi.object({
+    listing : Joi.object({
+        title: Joi.string().required(),
+        description : Joi.string().required(),
+        price :  Joi.number().required().min(0),
+        country : Joi.string().required(),
+        location : Joi.string().required(),
+        image : Joi.string().allow("",null),
     }).required()
 });
