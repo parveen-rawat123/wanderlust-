44a9359b6198edf83409a527182cbb90718fdfa2
praveen singh <rawatpraveen477@gmail.com>
2024-02-25 21:15:49 +0530

    last commit

--- app.js
+++ app.js
@@ -1,67 +1,72 @@
-if(process.env.NODE_ENV != "production"){
-  require('dotenv').config();
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
-const methodOverride = require('method-override');
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const listingsRouter = require("./routers/listing.js");
-const reviewRouter = require("./routers/reviews.js")
-const session = require("express-session")
+const reviewRouter = require("./routers/reviews.js");
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 const userRouter = require("./routers/user.js");
 const listing = require("./models/listing");
-const wrapAsync = require('./utils/wrapAsync.js');
-const { env } = require('process');
-const { log } = require('console');
-const dbUrl =process.env.ATLASDB_URL;
-
+const wrapAsync = require("./utils/wrapAsync.js");
+const { env } = require("process");
+const { log } = require("console");
+const dbUrl = process.env.ATLASDB_URL;
 
 async function main() {
-  await mongoose.connect(dbUrl)
-  .then(()=>{
-    console.log("connection success")
-  })
-  .catch((e)=>{
-    console.log("connection feild")
-  })
-};
+  await mongoose
+    .connect(dbUrl)
 
+}
 
 main()
   .then(() => {
-    console.log("connected to DB")
+    console.log("connected to DB");
   })
   .catch((err) => {
-    console.log(err)
+    console.log(err);
   });
-
-  
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, "/public")))
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
+
+const store = MongoStore.create({
+  mongoUrl:dbUrl,
+  crypto :{
+    secret : process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
+
+store.on("error",()=>{
+  console.log("ERROR in MONGO SESSION STORE",err)
+});
 
 
 const sessionoptions = {
-  secret : "mysupersecret",
-  resave : false,
-  saveUninitialized : true,
-  cookie : {
-    expires : Date.now() + 7 * 24 * 60 * 1000, 
-    maxAge :  7 * 24 * 60 * 1000,
-    httpOnly : true
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 1000,
+    httpOnly: true,
   },
 };
 
@@ -77,15 +82,12 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
-  res.locals.success = req.flash("success")
-  res.locals.error = req.flash("error")
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
   res.locals.currUser = req.user;
-  next()
+  next();
 });
-
-
-
 
 // app.get("/demouser",async (req,res)=>{
 //  let fakeuser = new User({
@@ -97,19 +99,17 @@ app.use((req,res,next)=>{
 //  res.send(registeredUser);
 // });
 
-
-app.use("/listings", listingsRouter)
+app.use("/listings", listingsRouter);
 app.use("/listings/:id/review", reviewRouter);
-app.use("/",userRouter)
-
+app.use("/", userRouter);
 
 // search route
 app.get("/search", async (req, res) => {
   let value = req.query.inputval;
   let findval;
 
-   if (!value || value.trim() === "") {
-     res.redirect("/listing/index.ejs");
+  if (!value || value.trim() === "") {
+    res.redirect("/listing/index.ejs");
   }
   if (!isNaN(value)) {
     findval = await listing.find({ price: { $eq: parseInt(value) } });
@@ -122,17 +122,16 @@ app.get("/search", async (req, res) => {
       ],
     });
   }
-if(findval.length===0){
-  res.render("./listing/notsearch.ejs")
-}else{
-  res.render("./listing/search.ejs", { findval });
-}
+  if (findval.length === 0) {
+    res.render("./listing/notsearch.ejs");
+  } else {
+    res.render("./listing/search.ejs", { findval });
+  }
 });
-
 
 // error handling for all route
 app.all("*", (req, res, next) => {
-  next(new ExpressError(404, "Page Not Found"))
+  next(new ExpressError(404, "Page Not Found"));
 });
 
 //error handler
@@ -142,6 +141,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(8080, () => {
-  console.log("server on listening on port 8080")
-})
-
+  console.log("server on listening on port 8080");
+});
